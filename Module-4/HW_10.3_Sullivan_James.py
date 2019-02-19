ab4afe4fa880edc591a6a71a84775e19624de2dd
James <jesullivan222@gmail.com>
2019-02-18 21:51:42 -0500

    Fixed prompt bug. Inner for loop was not updating user_int_list.

--- Module-4/HW_10.3_Sullivan_James.py
+++ Module-4/HW_10.3_Sullivan_James.py
@@ -8,21 +8,37 @@ occurrences of each.
 
 def count_numbers(integer_list):
 
+    print(integer_list)
+
     count_list = 100 * [0]
 
-    for number in integer_list:
+    for number in range(len(integer_list)):
 
-        count_list[number - 1] += 1
+        count_list[integer_list[number] - 1] += 1
 
     return count_list
 
 
 def prompt_input():
 
-    input_int_list = [int(input_int) for input_int in input(
-        'Enter integers (1-100), separated by spaces: ').split()]
+    while True:
+        try:
+            user_int_list = [int(input_int) for input_int in input(
+                'Enter integers (1-100), separated by spaces: ').split()]
 
-    return input_int_list
+            for element in user_int_list:
+
+                if element not in range(1, 101):
+                    print('One or more numbers entered are outside the required'
+                          ' range. Please enter numbers between 1 and 100.')
+                    user_int_list = prompt_input()
+
+            break
+
+        except ValueError:
+            print('Not a valid input. Please enter an integer between 1-100.')
+
+    return user_int_list
 
 
 def display_count(count_list):
@@ -38,28 +54,7 @@ def display_count(count_list):
             print(current_integer, 'occurs', count, 'times')
 
 
-while True:
-
-    try:
-        user_int_list = prompt_input()
-
-        for element in user_int_list:
-
-            if element not in range(1, 101):
-                print('One or more numbers entered are outside the required'
-                      ' range. Please enter number between 1 and 100.')
-                user_int_list = prompt_input()
-
-        break
-
-    except ValueError:
-        print('Not a valid input. Please enter an integer between 1-100.')
-        user_int_list = prompt_input()
-
-
-print(user_int_list)
-
-final_count_list = count_numbers(user_int_list)
+final_count_list = count_numbers(prompt_input())
 
 print(final_count_list)
 
