ed903cc1fac4db4701b5b06d10d97cbb8cfae1e3
James <jesullivan222@gmail.com>
2019-02-19 22:50:58 -0500

    Completed 14.1 with comments.

---
+++ Module-4/HW_14.1_Sullivan_James.py
@@ -0,0 +1,48 @@
+"""
+HW 14.1
+(Display keywords)
+Revise Listing 14.4 CountKeywords.py to display the key-words in a Python source
+file as well as to count the number of keywords.
+"""
+
+import os.path
+import sys
+
+
+def main():
+
+    keyWords = {'and', 'as', 'break', 'class',
+                'continue', 'def', 'del', 'elif', 'else'
+                'except', 'False', 'finally', 'for', 'from'
+                'global', 'if', 'import', 'in', 'is', 'lambda',
+                'None', 'nonlocal', 'not', 'or', 'pass', 'raise',
+                'return', 'True', 'try', 'while', 'with', 'yield'}
+
+    filename = input("Enter a Python source code filename: ").strip()
+
+    if not os.path.isfile(filename):  # Check if file exists
+        print('File', filename, 'does not exist')
+        sys.exit()
+
+    infile = open(filename, 'r')  # Read and split words from the file
+
+    text = infile.read().split()  # Read and split words from the file
+
+    keyWord_dict = {}
+
+    # Create dictionary w/ keyWords as keys, and count as value
+    for word in keyWords:
+        keyWord_dict[word] = 0
+
+    # Increment count value if key word is in text
+    for word in text:
+        if word in keyWords:
+            keyWord_dict[word] += 1
+
+    # Print keyWords that were found in text, along with counts
+    for key in keyWord_dict:
+        if keyWord_dict[key] != 0:
+            print(key, 'occurs', keyWord_dict[key], 'time(s).')
+
+
+main()
