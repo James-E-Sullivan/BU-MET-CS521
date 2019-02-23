9d3f4e3818f2dc9f671723e6fb478d38401bf32c
James <jesullivan222@gmail.com>
2019-02-22 20:13:40 -0500

    Completed HW 6.5. Need to add comments.

--- Module 5/HW_6.5_Sullivan_James.py
+++ Module 5/HW_6.5_Sullivan_James.py
@@ -11,3 +11,28 @@ the function to display them in increasing order. Here are some sample runs:
 Enter three numbers: 3, 2.4, 5
 The sorted numbers are 2.4 3 5
 """
+
+
+def displaySortedNumbers(num1, num2, num3):
+
+    numbers = [num1, num2, num3]
+    numbers.sort()
+    print('The sorted numbers are', end=' ')
+    for i in numbers:
+        print(i, end=' ')
+
+
+while True:
+
+    try:
+        user_numbers = [x for x in input('Enter three numbers: ').strip().split()]
+        if len(user_numbers) is not 3:
+            continue
+        else:
+            break
+
+    except ValueError:
+        continue
+
+displaySortedNumbers(user_numbers[0], user_numbers[1], user_numbers[2])
+
