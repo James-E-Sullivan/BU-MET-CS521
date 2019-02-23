0bc475766c541bbf4e5328070325563924e8a554
James <jesullivan222@gmail.com>
2019-02-23 12:51:54 -0500

    Added comments to HW 6.5.

--- Module 5/HW_6.5_Sullivan_James.py
+++ Module 5/HW_6.5_Sullivan_James.py
@@ -14,7 +14,10 @@ The sorted numbers are 2.4 3 5
 
 
 def displaySortedNumbers(num1, num2, num3):
-
+    """
+    Takes in 3 input numbers in any order, sorts them, then prints
+    the sorted numbers on one line
+    """
     numbers = [num1, num2, num3]
     numbers.sort()
     print('The sorted numbers are', end=' ')
@@ -25,14 +28,22 @@ def displaySortedNumbers(num1, num2, num3):
 while True:
 
     try:
-        user_numbers = [x for x in input('Enter three numbers: ').strip().split()]
+        # List comprehension appends input numbers into list
+        user_numbers = [x for x in input(
+            'Enter three numbers: ').strip().split()]
+
+        # If the length of the list is not 3, user prompted again
         if len(user_numbers) is not 3:
             continue
+
+        # If length of list is 3, program continues
         else:
             break
 
+    # If user does not input a number, user prompted again
     except ValueError:
         continue
 
+# The three user numbers are input into displaySortedNumbers function
 displaySortedNumbers(user_numbers[0], user_numbers[1], user_numbers[2])
 
